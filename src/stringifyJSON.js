4fc2912f4821d4f7f0afe71cfba7e86ca9b2699e
Nicholas Miron <nicholasmiron@gmail.com>
2019-01-31 16:56:31 -0500

    Added a method for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var newArr = [];
+  if(typeof obj === "string") return '"' + obj + '"';
+  else if(typeof obj === "number" || typeof obj === "boolean") {
+    return obj.toString();
+  }
+  else if(obj === null) return 'null';
+  else if(Array.isArray(obj)) {
+    if(Array.length === 0) return '[]';
+    else {
+      obj.forEach(function(val) {
+        newArr.push(stringifyJSON(val));
+      });
+      return '[' + newArr + ']';
+    }
+  } 
+  else if(typeof obj === "object") {
+  if(Object.keys(obj).length === 0) return '{}';
+  else {
+    Object.keys(obj).forEach(function(key) {
+      if(typeof obj[key] !== "undefined" && !(obj[key] instanceof Function)) {
+        newArr.push('"' + key + '":' + stringifyJSON(obj[key]) )
+      }
+    })
+    return '{' + newArr + '}';
+  } 
+
+  }
 };
